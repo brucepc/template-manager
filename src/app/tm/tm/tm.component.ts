da425cd9bd32f97e8319e9eff1ef420e9f96b8c4
Bruce P Carvalho <bruce.carvalho@citsamazonas.org.br>
2019-10-15 13:57:05 -0400

    add pagesize

--- src/app/tm/tm/tm.component.ts
+++ src/app/tm/tm/tm.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+import { CKEditorComponent, CKEditor5 } from '@ckeditor/ckeditor5-angular';
 
 @Component({
   selector: 'blockchain-tm',
   templateUrl: './tm.component.html',
   styleUrls: ['./tm.component.scss']
 })
-export class TmComponent implements OnInit {
+export class TmComponent implements OnInit, AfterViewInit {
   editorBuild = DecoupledEditor;
   editorConfig = {
     language: 'pt-br'
   };
   editorData = 'Testandoooooooooooo';
 
-  constructor() { }
+  @ViewChild('.ck.ck-content', { static: false }) contentRef: ElementRef;
+  @ViewChild(CKEditorComponent, { static: false }) ckComponentRef: CKEditorComponent;
+
+  constructor(
+    private render: Renderer2
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    console.log(this.contentRef);
+    this.ckComponentRef.ready.subscribe((editor: CKEditor5.Editor) => {
+      console.log(editor);
+      console.log(editor.sourceElement);
+      this.render.setStyle(editor.sourceElement, 'min-height', '297mm');
+      this.render.setStyle(editor.sourceElement, 'min-width', '210mm');
+    });
+
+    this.ckComponentRef.focus.subscribe((editor: CKEditor5.Editor) => {
+      this.render.setStyle(editor.sourceElement, 'min-height', '297mm');
+      this.render.setStyle(editor.sourceElement, 'min-width', '210mm');
+    });
+
   }
 
   public onReady(editor) {
